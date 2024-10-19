c45c09ea28e86b3adfc04f1ba05bd0c4cb5f98c5
LukeyDev <lukemadsen501@gmail.com>
2024-10-20 11:51:33 +1300

    Restructure + UI Space
    Restructuring the code to be easier to navigate, also implemented a "renderHeight" to use for rendering instead of screenHeight, so that there can be a gap below the viewport for UI

--- RaycastRenderer/Main.cpp
+++ RaycastRenderer/Main.cpp
@@ -10,9 +10,23 @@
 #define mapWidth 24
 #define mapHeight 24
 #define screenWidth 640
-#define screenHeight 480
+#define screenHeight 600
+#define renderHeight 480
 
 int worldMap[mapWidth][mapHeight];
+
+SDL_Surface* screenSurface = NULL;
+
+// Player variables
+double posX = 2, posY = 2;
+double dirX = -1, dirY = 0;
+double planeX = 0, planeY = 0.66;
+double moveSpeed = 5.0f;
+double rotSpeed = 2.2f;
+
+const int textureSize = 64;
+const int wallTypes = 9; // Must always be higher than the actual amount of tile textures, as air (0) counts as a wall type
+SDL_Surface* wallTextures[wallTypes];
 
 void loadMap(const std::string& filename) {
     std::ifstream file(filename);
@@ -123,32 +137,130 @@ void setPixel(SDL_Surface* surface, int x, int y, Uint32 color) {
     }
 }
 
+void Update(float deltaTime)
+{
+    // Clear the screen
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+
+    // Create the floor
+    SDL_Rect* floorRect = new SDL_Rect{ 0, renderHeight / 2, screenWidth, renderHeight / 2 };
+    SDL_FillRect(screenSurface, floorRect, SDL_MapRGB(screenSurface->format, 0x12, 0x12, 0x12));
+    delete(floorRect);
+
+    // Raycast
+    for (int x = 0; x < screenWidth; x++)
+    {
+        double cameraX = 2 * x / (double)screenWidth - 1;
+        double rayDirX = dirX + planeX * cameraX;
+        double rayDirY = dirY + planeY * cameraX;
+
+        int mapX = int(posX);
+        int mapY = int(posY);
+
+        double sideDistX;
+        double sideDistY;
+
+        double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
+        double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
+
+        double perpWallDist;
+
+        int stepX;
+        int stepY;
+
+        int hit = 0;
+        int side;
+
+        if (rayDirX < 0)
+        {
+            stepX = -1;
+            sideDistX = (posX - mapX) * deltaDistX;
+        }
+        else
+        {
+            stepX = 1;
+            sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+        }
+        if (rayDirY < 0)
+        {
+            stepY = -1;
+            sideDistY = (posY - mapY) * deltaDistY;
+        }
+        else
+        {
+            stepY = 1;
+            sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+        }
+
+        // DDA
+        while (hit == 0)
+        {
+            if (sideDistX < sideDistY)
+            {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+            }
+            else
+            {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+            }
+
+            if (worldMap[mapX][mapY] > 0)
+            {
+                hit = worldMap[mapX][mapY];
+                if (hit > wallTypes) hit = 1;
+            }
+        }
+
+        if (side == 0) perpWallDist = (sideDistX - deltaDistX);
+        else           perpWallDist = (sideDistY - deltaDistY);
+
+        int lineHeight = (int)(renderHeight / perpWallDist);
+
+        int drawStart = -lineHeight / 2 + renderHeight / 2;
+        if (drawStart < 0) drawStart = 0;
+        int drawEnd = lineHeight / 2 + renderHeight / 2;
+        if (drawEnd >= renderHeight) drawEnd = renderHeight - 1;
+
+        double wallX; // Exactly where the wall was hit
+        if (side == 0) wallX = posY + perpWallDist * rayDirY;
+        else           wallX = posX + perpWallDist * rayDirX;
+        wallX -= floor((wallX));
+
+        float verticleScale = (float)lineHeight / (float)textureSize;
+        int sampleX = (int)floor((wallX * textureSize)) % textureSize;
+
+        for (int y = 0; y < lineHeight; y++)
+        {
+            if (y + (renderHeight / 2) - (lineHeight / 2) > renderHeight) continue;
+            if (y + (renderHeight / 2) - (lineHeight / 2) < 0) continue;
+            int sampleY = (int)floor(y / verticleScale);
+
+            SDL_Color rgb = getPixelColor(wallTextures[hit], sampleX, sampleY);
+            setPixel(screenSurface, x, y + (renderHeight / 2) - (lineHeight / 2), SDL_MapRGB(screenSurface->format, rgb.r, rgb.g, rgb.b));
+        }
+    }
+}
+
 int main(int argc, char* args[])
 {
     loadMap("maps/2.rmap");
-
-	double posX = 2, posY = 2;
-    double dirX = -1, dirY = 0;
-	double planeX = 0, planeY = 0.66;
-    double moveSpeed = 5.0f;
-    double rotSpeed = 2.0f;
 
     bool movingForward = false;
     bool movingBackward = false;
     bool turningRight = false;
     bool turningLeft = false;
 
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
+    SDL_Window* window = NULL;
 
     SDL_Event event;
 
-    const int textureSize = 64;
-    const int wallTypes = 9; // Must always be higher than the actual amount of tile textures, as air (0) counts as a wall type
-    SDL_Surface* wallTextures[wallTypes];
-
+    // Load wall textures
     for (int i = 1; i < wallTypes; i++) {
-        std::string fileName = "walls/tile_" + std::to_string(i) + ".bmp";       
+        std::string fileName = "walls/tile_" + std::to_string(i) + ".bmp";
         wallTextures[i] = SDL_LoadBMP(fileName.c_str());
         if (!wallTextures[i]) {
             std::cerr << "Failed to load wall texture! SDL_Error: " << SDL_GetError() << std::endl;
@@ -158,147 +270,46 @@ int main(int argc, char* args[])
         }
     }
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+    // Init
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
 
-			SDL_UpdateWindowSurface(window);
-		}
-	}
+            SDL_UpdateWindowSurface(window);
+        }
+    }
 
     Uint64 NOW = SDL_GetPerformanceCounter();
     Uint64 LAST = 0;
     double deltaTime = 0;
 
+    // Main loop
     bool done = false;
-	while (!done)
-	{
+    while (!done)
+    {
         LAST = NOW;
         NOW = SDL_GetPerformanceCounter();
         deltaTime = (double)((NOW - LAST) * 1 / (double)SDL_GetPerformanceFrequency());
 
-        // Clear the screen
-        SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00)); 
-
-        // Create the floor
-        SDL_Rect* floorRect = new SDL_Rect{ 0, screenHeight / 2, screenWidth, screenHeight / 2 }; 
-        SDL_FillRect(screenSurface, floorRect, SDL_MapRGB(screenSurface->format, 0x12, 0x12, 0x12));
-        delete(floorRect);
-
-        // Raycast
-        for (int x = 0; x < screenWidth; x++)
-        {
-            double cameraX = 2 * x / (double)screenWidth - 1;
-            double rayDirX = dirX + planeX * cameraX;
-            double rayDirY = dirY + planeY * cameraX;
-
-            int mapX = int(posX);
-            int mapY = int(posY);
-
-            double sideDistX;
-            double sideDistY;
-
-            double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
-            double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
-
-            double perpWallDist;
-
-            int stepX;
-            int stepY;
-
-            int hit = 0;
-            int side;
-
-            if (rayDirX < 0)
-            {
-                stepX = -1;
-                sideDistX = (posX - mapX) * deltaDistX;
-            }
-            else
-            {
-                stepX = 1;
-                sideDistX = (mapX + 1.0 - posX) * deltaDistX;
-            }
-            if (rayDirY < 0)
-            {
-                stepY = -1;
-                sideDistY = (posY - mapY) * deltaDistY;
-            }
-            else
-            {
-                stepY = 1;
-                sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-            }
-
-            // DDA
-            while (hit == 0)
-            {
-                if (sideDistX < sideDistY)
-                {
-                    sideDistX += deltaDistX;
-                    mapX += stepX;
-                    side = 0;
-                }
-                else
-                {
-                    sideDistY += deltaDistY;
-                    mapY += stepY;
-                    side = 1;
-                }
-
-                if (worldMap[mapX][mapY] > 0)
-                {
-                    hit = worldMap[mapX][mapY];
-                    if (hit > wallTypes) hit = 1;
-                }
-            }
-
-            if (side == 0) perpWallDist = (sideDistX - deltaDistX);
-            else           perpWallDist = (sideDistY - deltaDistY);
-
-            int lineHeight = (int)(screenHeight / perpWallDist);
-
-            int drawStart = -lineHeight / 2 + screenHeight / 2;
-            if (drawStart < 0) drawStart = 0;
-            int drawEnd = lineHeight / 2 + screenHeight / 2;
-            if (drawEnd >= screenHeight) drawEnd = screenHeight - 1;
-
-            double wallX; // Exactly where the wall was hit
-            if (side == 0) wallX = posY + perpWallDist * rayDirY;
-            else           wallX = posX + perpWallDist * rayDirX;
-            wallX -= floor((wallX));
-            
-            float verticleScale = (float)lineHeight / (float)textureSize;
-            int sampleX = (int)floor((wallX * textureSize)) % textureSize;
-
-            for (int y = 0; y < lineHeight; y++)
-            {   
-                if (y + (screenHeight / 2) - (lineHeight / 2) > screenHeight) continue;
-                if (y + (screenHeight / 2) - (lineHeight / 2) < 0) continue;
-                int sampleY = (int)floor(y / verticleScale);
-                
-                SDL_Color rgb = getPixelColor(wallTextures[hit], sampleX, sampleY);
-                setPixel(screenSurface, x, y + (screenHeight / 2) - (lineHeight / 2), SDL_MapRGB(screenSurface->format, rgb.r, rgb.g, rgb.b));
-            }           
-        }
+        Update(deltaTime);
 
         SDL_UpdateWindowSurface(window);
 
         double oldDirX = dirX;
         double oldPlaneX = planeX;
-        
+
         // Input
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_KEYDOWN)
@@ -343,13 +354,13 @@ int main(int argc, char* args[])
                 default:
                     break;
                 }
-            }           
+            }
             if (event.type == SDL_QUIT) done = true;
         }
 
         // Applying input
         if (movingForward)
-        {          
+        {
             if (worldMap[int(posX + dirX * moveSpeed * deltaTime)][int(posY)] == false) posX += dirX * moveSpeed * deltaTime;
             if (worldMap[int(posX)][int(posY + dirY * moveSpeed * deltaTime)] == false) posY += dirY * moveSpeed * deltaTime;
         }
@@ -372,10 +383,10 @@ int main(int argc, char* args[])
             planeX = planeX * cos(rotSpeed * deltaTime) - planeY * sin(rotSpeed * deltaTime);
             planeY = oldPlaneX * sin(rotSpeed * deltaTime) + planeY * cos(rotSpeed * deltaTime);
         }
-	}
+    }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
 
-	return 0;
+    return 0;
 }
